817c2fd86355162d3c28ed0929b6e316d2d0e97a
niiabe <niiabeabbey@gmail.com>
2025-08-05 23:21:42 +0000

    testing

---
+++ resources/assets/js/main.js
@@ -0,0 +1,28 @@
+
+
+(function() {
+  "use strict";
+
+ 
+  /**
+   * Animation on scroll function and init
+   */
+  function aosInit() {
+    AOS.init({
+      duration: 600,
+      easing: 'ease-in-out',
+      once: true,
+      mirror: false
+    });
+  }
+  window.addEventListener('load', aosInit);
+
+  /**
+   * Initiate glightbox
+   */
+  const glightbox = GLightbox({
+    selector: '.glightbox'
+  });
+
+  
+})();
